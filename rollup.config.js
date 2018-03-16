c2666f4c642bd824eb22adfbf3d6e684f24e4c6f
vasco <vastur@gmail.com>
2018-03-15 23:07:55 -0700

    Bump dev deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,19 @@ import babel from 'rollup-plugin-babel';
 import { name, homepage, version } from './package.json';
 
 export default {
-    external: ['react'],
-    globals: { react: 'React' },
-    input: 'index.js',
-    output: [
-        {
-            format: 'umd',
-            name: 'fromKapsule',
-            file: `dist/${name}.js`,
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        babel()
-    ],
-    banner: `// Version ${version} ${name} - ${homepage}`
+  external: ['react'],
+  input: 'index.js',
+  output: [
+    {
+      format: 'umd',
+      name: 'fromKapsule',
+      file: `dist/${name}.js`,
+      globals: { react: 'React' },
+      sourcemap: true,
+      banner: `// Version ${version} ${name} - ${homepage}`
+    }
+  ],
+  plugins: [
+    babel()
+  ]
 };
