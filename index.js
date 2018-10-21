b7e2cc9ef16bad733f6a59a441617276cc267e2d
vasco <vastur@gmail.com>
2018-10-21 03:11:15 -0700

    Properly apply initProps

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-export default function(kapsuleComponent, wrapperElType = 'div', bindMethodNames = [], configOptions) {
+export default function(kapsuleComponent, wrapperElType = 'div', bindMethodNames = [], initProps = []) {
   class FromKapsuleComp extends React.PureComponent {
-    state = {
-      comp: kapsuleComponent(configOptions)
-    };
+    constructor(props) {
+      super(props);
+
+      const configOptions = Object.assign({}, ...initProps
+        .filter(p => props.hasOwnProperty(p))
+        .map(prop => ({ [prop]: props[prop] }))
+      );
+
+      this.state = {
+        comp: kapsuleComponent(configOptions)
+      }
+    }
 
     // Call a component method
     _call = (method, ...args) =>
@@ -12,15 +21,21 @@ export default function(kapsuleComponent, wrapperElType = 'div', bindMethodNames
         ? this.state.comp[method](...args)
         : undefined; // method not found
 
+    _getDynamicProps = () => {
+      const dynamicProps = Object.assign({}, this.props);
+      initProps.forEach(initProp => delete dynamicProps[initProp]); // initProps should not be called
+      return dynamicProps;
+    };
+
     componentDidMount() {
-      Object.keys(this.props).forEach(p => {
+      Object.keys(this._getDynamicProps()).forEach(p => {
         this._call(p, this.props[p]);
       });
       this.state.comp(this.rootElem);
     }
 
     componentDidUpdate(prevProps) {
-      Object.keys(this.props).forEach(p => {
+      Object.keys(this._getDynamicProps()).forEach(p => {
         if (prevProps[p] !== this.props[p]) {
           this._call(p, this.props[p]);
         }
